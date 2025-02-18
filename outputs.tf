7ede2c4ef38a8357cf720fa2972a2709fc3a6fe6
dua00006 <dua00006@algonquinlive.com>
2025-02-17 20:35:05 -0500

    Add descriptions to output variables, rename labelPrefix to label_prefix, add description and type to region variable in variables.tf and outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,19 @@
-# Define output values for later reference
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value       = azurerm_resource_group.example.name
+  description = "The name of the Azure Resource Group."
 }
 
 output "vm_name" {
-  value = azurerm_linux_virtual_machine.webserver.name
+  value       = azurerm_virtual_machine.example.name
+  description = "The name of the Virtual Machine."
 }
 
 output "nic_name" {
-  value = azurerm_network_interface.webserver.name
+  value       = azurerm_network_interface.example.name
+  description = "The name of the Network Interface Card."
 }
 
 output "public_ip" {
-  value = azurerm_linux_virtual_machine.webserver.public_ip_address
+  value       = azurerm_public_ip.example.ip_address
+  description = "The public IP address of the virtual machine."
 }
